6f5c4fb21c715981e7815e3df89e039a33850fea
JenniferTembladorAguilar <jennifer-andrea.temblador@atos.net>
2022-07-22 08:13:33 -0500

    2. Show all the countries on a table
    Show all the countries on a table
    Validate data

--- countriesController.js
+++ countriesController.js
@@ -2,54 +2,69 @@
 // api url 
 const api_url = "https://restcountries.com/v3.1/all";
 
- 
-async function getApi(url) {
-	
-	
-	const response = await fetch(url);
-	
-	// Store allCountries in JSON 
-	var allCountries = await response.json();
 
-	console.log(allCountries);
-	if (response) {
-		 
-	}
-	show(allCountries);
+async function getApi(url) {
+
+
+    const response = await fetch(url);
+
+    // Store allCountries in JSON 
+    var allCountries = await response.json();
+
+    console.log(allCountries);
+    if (response) {
+
+    }
+    show(allCountries);
 }
- 
+
 getApi(api_url);
 
+
+
  
- 
-// Function to define innerHTML for HTML table
 function show(allCountries) {
-	let tab =
-		`<tr>
-		<th>Official Name</th>
-        <th>Capital</th>
-        <th>Region</th>
-        <th>Language</th>
-        <th>Population</th>
-        <th>Flag</th>
+    let tab =
+        `<tr>
+		<th>OFFICIAL NAME</th>
+        <th>CAPITAL</th>
+        <th>REGION</th>
+        <th>LANGUAGE</th>
+        <th>POPULATION</th>
+        <th>FLAG</th>
 		</tr>`;
-	
-	// Loop to access all rows
-	for (let country in allCountries) {
-     
-     
-   
-		tab += `<tr>
+
+    // Loop 
+    for (let country in allCountries) {
+
+        var valuesLanguage;
+        if (allCountries[country].languages) {
+            valuesLanguage = Object.values(allCountries[country].languages);
+            console.log(valuesLanguage);
+        } else {
+            valuesLanguage = 'No Language to display';
+        }
+
+        var valuesCapital;
+        if (allCountries[country].capital) {
+            valuesCapital = allCountries[country].capital;
+        } else {
+            valuesCapital = 'No Capital';
+
+        }
+
+
+        tab += `<tr>
 	<td>${allCountries[country].name.official} </td>
-    <td>${allCountries[country].capital} </td>
+    <td>${valuesCapital} </td>
     <td>${allCountries[country].region} </td>
-    <td>${allCountries[country].languages} </td>
+    <td>${valuesLanguage} </td>
     <td>${allCountries[country].population} </td>
     <td><img src="${allCountries[country].flags.png}"> </td>
     
 		
 </tr>`;
-	}
-	// Setting innerHTML as tab variable
-	document.getElementById("countriesTable").innerHTML = tab;
+    }
+     
+    document.getElementById("countriesTable").innerHTML = tab;
 }
